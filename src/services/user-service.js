33500ae9db743b90cba78a5681d30883f6fe0802
codder077 <120477792+codder077@users.noreply.github.com>
2024-05-20 18:27:57 +0530

    token verfication

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,9 @@
 
 
+const { JWT_KEY } = require('../config/serverConfig');
 const UserRepository = require('../repository/user-repository');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
 class UserService {
@@ -17,6 +20,72 @@ class UserService {
             throw error;
         }
     }
+
+
+    async signIn(email, plainPassword) {
+        try {
+            const user = await this.userRepository.getByEmail(email);
+            const passwordsMatch = this.checkPassword(plainPassword, user.password);
+
+            if(!passwordsMatch) {
+                console.log("Password doesn't match");
+                throw {error: 'Incorrect password'};
+            }
+            const newJWT = this.createToken({email: user.email, id: user.id});
+            return newJWT;
+        } catch (error) {
+            console.log("Something went wrong in the sign in process");
+            throw error;
+        }
+    }
+
+    async isAuthenticated(token) {
+        try {
+            const response = this.verifyToken(token);
+            if(!response) {
+                throw {error: 'Invalid token'}
+            }
+            const user = await this.userRepository.getById(response.id);
+            if(!user) {
+                throw {error: 'No user with the corresponding token exists'};
+            }
+            return user.id;
+        } catch (error) {
+            console.log("Something went wrong in the auth process");
+            throw error;
+        }
+    }
+
+    
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation", error);
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison");
+            throw error;
+        }
+    }
+
 
 }
 
